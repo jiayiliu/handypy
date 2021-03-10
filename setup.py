ff9f05f4129d53a727a5f4c31d5860d7c24631fe
jiayiliu <jiayiliu@kuaishou.com>
2021-03-09 20:31:42 -0800

    fix version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 long_description = open("README.md").read()
 
 setup(name='handypy',
-      version='0.2.1',
+      version='0.2.2',
       description='Handy Python scripts',
       long_description=long_description,
       long_description_content_type='text/markdown',
